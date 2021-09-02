9c639fd21efa334ac7e1e71bb3b7a836232f761e
naki27 <kyo.jin.go@gmail.com>
2021-09-03 00:05:05 +0900

    favicon fix

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -14,9 +14,9 @@ export default class MyDocument extends Document {
           <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
           <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
 
-          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+          <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png"/>
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png"/>
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png"/>
           <link rel="manifest" href="/favicons/site.webmanifest"/>
           <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"/>
           <meta name="msapplication-config" content="/favicons/browserconfig.xml" />
