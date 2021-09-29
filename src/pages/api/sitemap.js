2cb1e672ea38774102c02471a34b0b4a60e28dfa
naki27 <kyo.jin.go@gmail.com>
2021-09-29 22:20:45 +0900

    bugfix

--- src/pages/api/sitemap.js
+++ src/pages/api/sitemap.js
@@ -1,37 +1,37 @@
-import { SitemapStream, streamToPromise } from "./sitemap";
-import graphlqlFetch from "lib/apollo"
+// import { SitemapStream, streamToPromise } from "./sitemap";
+// import graphlqlFetch from "lib/apollo"
 
 export default async (req, res) => {
-  const { posts } = await graphlqlFetch(`
-    query getSitemapData {
-      projects: allWorks {
-        slug {
-          current
-        }
-        publishedAt
-      }
-    }
-  `);
+  // const { posts } = await graphlqlFetch(`
+  //   query getSitemapData {
+  //     projects: allWorks {
+  //       slug {
+  //         current
+  //       }
+  //       publishedAt
+  //     }
+  //   }
+  // `);
 
-  const smStream = new SitemapStream({
-    hostname: "https://priver.dev",
-  });
+  // const smStream = new SitemapStream({
+  //   hostname: "https://priver.dev",
+  // });
 
-  smStream.write({
-    url: "/",
-  });
+  // smStream.write({
+  //   url: "/",
+  // });
 
-  posts.forEach((element) => {
-    smStream.write({
-      url: `/${element.slug.current}`,
-      lastmod: element.publishedAt,
-    });
-  });
+  // posts.forEach((element) => {
+  //   smStream.write({
+  //     url: `/${element.slug.current}`,
+  //     lastmod: element.publishedAt,
+  //   });
+  // });
 
-  smStream.end();
-  const sitemap = await streamToPromise(smStream).then((sm) => sm.toString());
+  // smStream.end();
+  // const sitemap = await streamToPromise(smStream).then((sm) => sm.toString());
 
-  res.setHeader("Content-Type", "text/xml");
-  res.write(sitemap);
-  res.end();
+  // res.setHeader("Content-Type", "text/xml");
+  // res.write(sitemap);
+  // res.end();
 };
