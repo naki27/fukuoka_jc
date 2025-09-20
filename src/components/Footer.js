d62b1027cf5275fc9b407db45c1bbc9c516e92a9
naki27 <kyo.jin.go@gmail.com>
2025-09-20 19:10:27 +0900

    bugfix3

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,12 +10,9 @@ import Divider from "@mui/material/Divider";
 export default function ButtonAppBar({isMobile}) {
   return (
     <Box className={styles.footer} p={2} display="flex" justifyContent="space-evenly">
-      {isMobile ? <></>
-      :
-        <Box display="flex" flexDirection="column" alignItems="flex-start">
-          <p>
-            第32回  ジャパンカップビーチボール選手権福岡大会  公式サイト
-          </p>
+      {isMobile ?
+        <Box display="flex" flexDirection="column" alignItems="center" sx={{textAlign: 'center'}}>
+          <p>第32回ジャパンカップビーチボール選手権福岡大会<br />公式サイト</p>
           <Image src={logo} className={styles.logo} alt="第32回ジャパンカップビーチボール選手権福岡大会 公式サイト"/>
           <p>
             {'Copyright © '}
@@ -27,73 +24,88 @@ export default function ButtonAppBar({isMobile}) {
             {'.'}
           </p>
         </Box>
-      }
-      {isMobile ? <></>
       :
-        <Box display="flex" flexDirection="row">
-          <Box display="flex" flexDirection="column" mx={3}>
-            <MuiLink href="/tournamentGuidelines">
-              <p>{"大会要項"}</p>
-            </MuiLink>
-            {/* <MuiLink href="/detailGuidelines"> */}
-            <MuiLink href="/underConstruction">
-              <p>{"細部要項"}</p>
-            </MuiLink>
-            {/* <MuiLink href="/accessMap"> */}
-            <MuiLink href="/underConstruction">
-              <p>{"アクセスマップ"}</p>
-            </MuiLink>
-            <MuiLink href="/underConstruction">
-              <p>{"会場図"}</p>
-            </MuiLink>
-            {/* <MuiLink href="/variousGuides"> */}
-            <MuiLink href="/underConstruction">
-              <p>{"各種ご案内"}</p>
-            </MuiLink>
+        <>
+          <Box display="flex" flexDirection="column" alignItems="flex-start">
+            <p>
+              第32回  ジャパンカップビーチボール選手権福岡大会  公式サイト
+            </p>
+            <Image src={logo} className={styles.logo} alt="第32回ジャパンカップビーチボール選手権福岡大会 公式サイト"/>
+            <p>
+              {'Copyright © '}
+              <MuiLink href="https://fukuoka-beachball.jimdofree.com/">
+                  福岡県ビーチボール協会
+              </MuiLink>
+              {' '}
+              {new Date().getFullYear()}
+              {'.'}
+            </p>
           </Box>
-          <Box display="flex" flexDirection="column" mx={3}>
-            <Box display="flex" flexDirection="column">
-              {/* <MuiLink href="/matchTableIndex?sex=female"> */}
-              <MuiLink href="/underConstruction">
-                <p>{"組み合わせ（女子）"}</p>
+          <Box display="flex" flexDirection="row">
+            <Box display="flex" flexDirection="column" mx={3}>
+              <MuiLink href="/tournamentGuidelines">
+                <p>{"大会要項"}</p>
               </MuiLink>
-              {/* <MuiLink href="/matchTableIndex?sex=male"> */}
+              {/* <MuiLink href="/detailGuidelines"> */}
               <MuiLink href="/underConstruction">
-                <p>{"組み合わせ（男子）"}</p>
+                <p>{"細部要項"}</p>
+              </MuiLink>
+              {/* <MuiLink href="/accessMap"> */}
+              <MuiLink href="/underConstruction">
+                <p>{"アクセスマップ"}</p>
+              </MuiLink>
+              <MuiLink href="/underConstruction">
+                <p>{"会場図"}</p>
+              </MuiLink>
+              {/* <MuiLink href="/variousGuides"> */}
+              <MuiLink href="/underConstruction">
+                <p>{"各種ご案内"}</p>
               </MuiLink>
             </Box>
-            <Divider sx={{ borderColor: 'white', margin: '16px 0'}}/>
-            <Box display="flex" flexDirection="column">
-              {/* <MuiLink href="/progressChart?bf=league"> */}
-              <MuiLink href="/underConstruction">
-                <p>{"進行表（予選）"}</p>
-              </MuiLink>
-              {/* <MuiLink href="/progressChart?bf=tournament"> */}
-              <MuiLink href="/underConstruction">
-                <p>{"進行表（決勝）"}</p>
-              </MuiLink>
+            <Box display="flex" flexDirection="column" mx={3}>
+              <Box display="flex" flexDirection="column">
+                {/* <MuiLink href="/matchTableIndex?sex=female"> */}
+                <MuiLink href="/underConstruction">
+                  <p>{"組み合わせ（女子）"}</p>
+                </MuiLink>
+                {/* <MuiLink href="/matchTableIndex?sex=male"> */}
+                <MuiLink href="/underConstruction">
+                  <p>{"組み合わせ（男子）"}</p>
+                </MuiLink>
+              </Box>
+              <Divider sx={{ borderColor: 'white', margin: '16px 0'}}/>
+              <Box display="flex" flexDirection="column">
+                {/* <MuiLink href="/progressChart?bf=league"> */}
+                <MuiLink href="/underConstruction">
+                  <p>{"進行表（予選）"}</p>
+                </MuiLink>
+                {/* <MuiLink href="/progressChart?bf=tournament"> */}
+                <MuiLink href="/underConstruction">
+                  <p>{"進行表（決勝）"}</p>
+                </MuiLink>
+              </Box>
+            </Box>
+            <Box display="flex" flexDirection="column" mx={3}>
+              <Box display="flex" flexDirection="column">
+                {/* <MuiLink href="/progressChart?bf=league"> */}
+                <MuiLink href="/underConstruction">
+                  <p>{"大会結果（予選）"}</p>
+                </MuiLink>
+                {/* <MuiLink href="/progressChart?bf=tournament"> */}
+                <MuiLink href="/underConstruction">
+                  <p>{"大会結果（決勝）"}</p>
+                </MuiLink>
+              </Box>
+              <Divider sx={{ borderColor: 'white', margin: '16px 0'}}/>
+              <Box display="flex" flexDirection="column">
+                {/* <MuiLink href="/sponsor"> */}
+                <MuiLink href="/underConstruction">
+                  <p>{"スポンサー"}</p>
+                </MuiLink>
+              </Box>
             </Box>
           </Box>
-          <Box display="flex" flexDirection="column" mx={3}>
-            <Box display="flex" flexDirection="column">
-              {/* <MuiLink href="/progressChart?bf=league"> */}
-              <MuiLink href="/underConstruction">
-                <p>{"大会結果（予選）"}</p>
-              </MuiLink>
-              {/* <MuiLink href="/progressChart?bf=tournament"> */}
-              <MuiLink href="/underConstruction">
-                <p>{"大会結果（決勝）"}</p>
-              </MuiLink>
-            </Box>
-            <Divider sx={{ borderColor: 'white', margin: '16px 0'}}/>
-            <Box display="flex" flexDirection="column">
-              {/* <MuiLink href="/sponsor"> */}
-              <MuiLink href="/underConstruction">
-                <p>{"スポンサー"}</p>
-              </MuiLink>
-            </Box>
-          </Box>
-        </Box>
+        </>
       }
     </Box>
   );
