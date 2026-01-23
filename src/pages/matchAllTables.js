35d14bb60a66a26d08d4da1546899b9351aafb69
naki27 <kyo.jin.go@gmail.com>
2026-01-23 09:29:20 +0900

    組み合わせファイル更新

--- src/pages/matchAllTables.js
+++ src/pages/matchAllTables.js
@@ -20,7 +20,7 @@ const matchAllTables = () => {
     const [cacheBuster, setCacheBuster] = useState(Date.now());
     const [pdfLoaded, setPdfLoaded] = useState(false);
 
-    const basePdfUrl = `https://ijtbgl77pfz64mxz.public.blob.vercel-storage.com/%E7%AC%AC32%E5%9B%9E%E3%82%B7%E3%82%99%E3%83%A3%E3%83%8F%E3%82%9A%E3%83%B3%E3%82%AB%E3%83%83%E3%83%95%E3%82%9A%E7%A6%8F%E5%B2%A1%E5%A4%A7%E4%BC%9A_%E5%A4%A7%E4%BC%9A%E7%B5%84%E3%81%BF%E5%90%88%E3%82%8F%E3%81%9B_260112.pdf`;
+    const basePdfUrl = `https://ijtbgl77pfz64mxz.public.blob.vercel-storage.com/%E5%A4%A7%E4%BC%9A%E7%B5%84%E5%90%88%E3%81%9B.pdf`;
     const pdfFilePath = `${basePdfUrl}?t=${cacheBuster}`;
 
     pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
